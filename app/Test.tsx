fde4174a9a2513636af41f355d15bc7971b0ab52
Fredrik Ballestad <fba037@uib.no>
2023-10-31 15:28:09 +0100

    started project

---
+++ app/Test.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Test(){
+    return(
+        <div>
+            <Image src="/images/OIG.qVLyA2MiX9q.jpg" alt="Banklogo" width={200} height={200} />
+        </div>
+    )
+}
