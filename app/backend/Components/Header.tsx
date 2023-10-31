3e7c75b4d3931e6b91677a48db4a5b8d55cf437d
Fredrik Ballestad <fba037@uib.no>
2023-10-31 22:46:46 +0100

    made the picture in header centered

--- app/backend/Components/Header.tsx
+++ app/backend/Components/Header.tsx
@@ -1,20 +1,25 @@
 import Image from 'next/image';
 
-export default function Header(){
-    return (
-        <header className="bg-gray-800 p-4 flex items-center justify-between">
-          <div className="flex space-x-4">
-            <a href="/" className="text-white">Hjem</a>
-            <a href="/om" className="text-white">Om oss</a>
-            <a href="/kontakt" className="text-white">Kontakt</a>
-          </div>
-          <div className="text-center">
-            <img src="/images/OIG.qVLyA2MiX9q.jpg" alt="Logo" className="w-32 h-12" />
-          </div>
-          <div>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
-          </div>
-        </header>
-      );
+export default function Header() {
+  const imageStyle = {
+    marginRight: '140px', // Juster venstre margin etter dine preferanser
+  };
+
+  return (
+    <header className="bg-gray-800 p-4 flex items-center justify-between">
+      <div className="flex space-x-4">
+        <a href="/" className="text-white">Hjem</a>
+        <a href="/om" className="text-white" style={{ whiteSpace: 'nowrap' }}>Om oss</a>
+        <a href="/kontakt" className="text-white">Kontakt</a>
+      </div>
+      <div className="text-center flex items-center justify-center w-full">
+        <img src="/images/OIG.qVLyA2MiX9q.jpg" alt="Logo" className="w-20 h-12" style={imageStyle} />
+      </div>
+      <div>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
+      </div>
+    </header>
+  );
 }
+
 
